fb4a6fc42c704239314940548b4646c5e6cf3e8a
unknown <2612130038@ACADEMICO.IESB.BR>
2026-04-01 10:26:13 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main () {
+
+    double numero_decimal;
+
+    numero_decimal = 3.1415926;
+    printf("%f\n", numero_decimal);
+    printf("%0.2f\n", numero_decimal);
+
+        return 0;
+    }
